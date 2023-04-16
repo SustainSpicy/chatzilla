1daf5551db6473b785200bd2dd0cbed1171faeca
sustainspicy <isrealspicy@gmail.com>
2023-04-16 14:24:17 +0700

    code refractor

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,5 +1,5 @@
 const allowedOrigins = [
   "http://localhost:3000",
-  "https://friendly-haupia-098320.netlify.app/login",
+  "https://friendly-haupia-098320.netlify.app",
 ];
 export default allowedOrigins;
